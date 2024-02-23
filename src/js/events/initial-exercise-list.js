c06faecd8c3f26db1de63e3d2b4dc4a3072ef99a
Oleh Cheliakh <oleh.cheliakh@gmail.com>
2024-02-23 22:37:38 +0200

    Add breadcrumbs

--- src/js/events/initial-exercise-list.js
+++ src/js/events/initial-exercise-list.js
@@ -4,6 +4,7 @@ import { refs } from '../refs';
 
 import { getExercisesByKeyword } from '../api/get-exercises-by-keyword';
 import renderExercisesList from '../dom/render-exercises-list';
+import addText from '../helper/add-text';
 
 export default async function initialExercisesList() {
   let filter = filtersService.exercisesFilters.get();
@@ -16,6 +17,8 @@ export default async function initialExercisesList() {
   group = group.toLowerCase();
 
   const responseData = await getExercisesByKeyword({ filter, group });
+
+  addText(refs.breadcrumbsText, group, refs.breadcrumbsDivider);
   // Render exercises list
   renderExercisesList(refs.exercisesContainer, responseData);
 }
