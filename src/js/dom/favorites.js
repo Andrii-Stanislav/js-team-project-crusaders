634aa00503f0bafdb92293d8729c118945762048
999Ralex999 <140698368+999Ralex999@users.noreply.github.com>
2024-02-23 17:18:50 +0200

    fix error Refs

--- src/js/dom/favorites.js
+++ src/js/dom/favorites.js
@@ -1,6 +1,7 @@
 import { favoritesStorage } from '../storage/favorites';
 import { fetchExercise } from '../api/fetch-exercise';
 import createExercisesMarkup from './create-exercises-markup';
+import { refs } from '../refs';
 
 const favoriteExercises = favoritesStorage.getAll();
 
