3a6165a738a5d147862f99f1bfe1a235968c0571
Filip-Povidernyi <p.poviderniy@gmail.com>
2024-02-23 09:49:47 +0200

    ready for review

--- src/js/api/fetch-filters.js
+++ src/js/api/fetch-filters.js
@@ -1,7 +1,7 @@
 import { BASE_URL } from './config';
 
-export async function fetchFilters({ page, filter }) {
-  const URL = `${BASE_URL}/filters?page=${page}&filter=${filter}`;
+export async function fetchFilters({ page, param }) {
+  const URL = `${BASE_URL}/filters?page=${page}&filter=${param}&limit=12`;
 
   try {
     const res = await fetch(URL);
--- src/js/dom/modal-exercises.js
+++ src/js/dom/modal-exercises.js
@@ -1,194 +1,194 @@
-import spriteUrl from './../../images/svg/icons.svg';
-import { fetchExercise } from '../api/fetch-exercise';
-import Modal from '../helper/modal';
+// import spriteUrl from './../../images/svg/icons.svg';
+// import { fetchExercise } from '../api/fetch-exercise';
+// import Modal from '../helper/modal';
 
-document.getElementById('open-modal').addEventListener('click', () => {
-  modalExercises('64f389465ae26083f39b17a5');
-});
-
-export async function modalExercises(id, fn) {
-  const myModal = new Modal();
-
-  try {
-    const cardData = await fetchExercise(id);
-
-    myModal.modalContent.innerHTML = '';
-
-    myModal.modalContent.insertAdjacentHTML(
-      'beforeend',
-      createModalExercisesMarkup({
-        ...cardData,
-        rating: Number(cardData.rating).toFixed(1),
-      })
-    );
-    myModal.open();
-
-    myModal.modalContent.addEventListener('click', event => {
-      if (event.target.closest('.modal-exercises__button-favourites')) {
-        // handleClickFavoritesBtn(cardData);
-      }
-    });
-    myModal.modalContent.addEventListener('click', event => {
-      if (event.target.closest('.modal-exercises__button-rating')) {
-        myModal.close()
-        handleRatingCick(cardData._id);
-      }
-    });
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-export function createModalExercisesMarkup(cardData) {
-  const {
-    name,
-    burnedCalories,
-    bodyPart,
-    description,
-    target,
-    equipment,
-    gifUrl,
-    popularity,
-    rating,
-    time,
-    _id,
-  } = cardData;
-
-  return `<div class="modal-exercises__card" >
-    <div class="modal-exercises__image-wrapper">
-      <img class="modal-exercises__image" src="${
-        gifUrl !== null ? gifUrl : noImageUrl
-      }" alt="${name}" />
-    </div>
-    <div class="modal-exercises__description">
-      <p class="modal-exercises__name">${name}</p>
-      <div id="rating" class="modal-exercises__rating">
-        <div class="modal-exercises__rating-value">${rating}</div>
-        <span class="modal-exercises__rating-star" data-value="1">&#9733;</span>
-        <span class="modal-exercises__rating-star" data-value="2">&#9733;</span>
-        <span class="modal-exercises__rating-star" data-value="3">&#9733;</span>
-        <span class="modal-exercises__rating-star" data-value="4">&#9733;</span>
-        <span class="modal-exercises__rating-star" data-value="5">&#9733;</span>
-      </div>
-      <div class="modal-exercises__partials">
-        <div class="modal-exercises__partials-item">
-          <p class="modal-exercises__partials-title">Target</p>
-          <p class="modal-exercises__partials-value">${target}</p>
-        </div>
-        <div class="modal-exercises__partials-item">
-          <p class="modal-exercises__partials-title">Body Part</p>
-          <p class="modal-exercises__partials-value">${bodyPart}</p>
-        </div>
-        <div class="modal-exercises__partials-item">
-          <p class="modal-exercises__partials-title">Equipment</p>
-          <p class="modal-exercises__partials-value">${equipment}</p>
-        </div>
-        <div class="modal-exercises__partials-title">
-          <p class="modal-exercises__partials-title">Popular</p>
-          <p class="modal-exercises__partials-value">${popularity}</p>
-        </div>
-        <div class="modal-exercises__partials-item">
-          <p class="modal-exercises__partials-title">Burned calories</p>
-          <p class="modal-exercises__partials-value">${burnedCalories}/${time} 
-            <span class="modal-exercises__partials-value_span">min</span>
-          </p>
-        </div>
-      </div>
-      <p class="modal-exercises__text">
-        ${description}
-      </p>
-      <div class="modal-exercises__buttons">
-      ${
-        false
-          ? `<button 
-          type="button"
-          class="modal-exercises__button-favourites unfavorite-btn"
-          >
-          Unfavorite
-          <svg
-            class="modal-exercises__button-favourites_icon"
-            aria-label='heart'
-            width="20"
-            height="20"
-          >
-            <use href=${spriteUrl}#icon-trash></use>
-          </svg>`
-          : `<button 
-          type="button"
-          class="modal-exercises__button-favourites
-          add-to-favorites-btn">
-          Add to favorites
-          <svg
-            class="modal-exercises__button-favourites_icon"
-            aria-label='heart'
-            width="20"
-            height="20"
-          >
-            <use href=${spriteUrl}#icon-heart></use>
-          </svg>`
-      }
-        </button>
-        <button class="modal-exercises__button-rating" data-value="${_id}">Give a rating</button>
-      </div>
-    </div>
-    </div>`;
-}
-
-export function handleRatingCick(id) { 
-  
-}
-
-// const exercisesListRef = document.getElementById('exercises-list-container');
-
-// if (exercisesListRef) {
-//   exercisesListRef.addEventListener('click', onExerciseListClick);
-// }
-
-// export function onExerciseListClick(event) {
-
-//   if (!event.target.closest('[data-id]')) {
-//     return;
-//   }
-
-//   const id = event.target.closest('[data-id]').dataset.id;
+// document.getElementById('open-modal').addEventListener('click', () => {
 //   modalExercises('64f389465ae26083f39b17a5');
+// });
+
+// export async function modalExercises(id, fn) {
+//   const myModal = new Modal();
+
+//   try {
+//     const cardData = await fetchExercise(id);
+
+//     myModal.modalContent.innerHTML = '';
+
+//     myModal.modalContent.insertAdjacentHTML(
+//       'beforeend',
+//       createModalExercisesMarkup({
+//         ...cardData,
+//         rating: Number(cardData.rating).toFixed(1),
+//       })
+//     );
+//     myModal.open();
+
+//     myModal.modalContent.addEventListener('click', event => {
+//       if (event.target.closest('.modal-exercises__button-favourites')) {
+//         // handleClickFavoritesBtn(cardData);
+//       }
+//     });
+//     myModal.modalContent.addEventListener('click', event => {
+//       if (event.target.closest('.modal-exercises__button-rating')) {
+//         myModal.close()
+//         handleRatingCick(cardData._id);
+//       }
+//     });
+//   } catch (error) {
+//     console.error(error);
+//   }
 // }
 
+// export function createModalExercisesMarkup(cardData) {
+//   const {
+//     name,
+//     burnedCalories,
+//     bodyPart,
+//     description,
+//     target,
+//     equipment,
+//     gifUrl,
+//     popularity,
+//     rating,
+//     time,
+//     _id,
+//   } = cardData;
 
-
-// export const handleClickFavoritesBtn = cardData => {
-//   const favoriteButton = document.querySelector(
-//     '.modal-exercises__button-favourites'
-//   );
-//   const isFavorite = isFavorite(cardData._id)
-//   if (!isFavorite) {
-//     // TODO: addFavoriteCardToLocal(cardData);
-
-//     favoriteButton.innerHTML = `Unfavorite
-//     <svg
-//             class="modal-exercises__button-favourites_icon unfavorite-btn"
+//   return `<div class="modal-exercises__card" >
+//     <div class="modal-exercises__image-wrapper">
+//       <img class="modal-exercises__image" src="${
+//         gifUrl !== null ? gifUrl : noImageUrl
+//       }" alt="${name}" />
+//     </div>
+//     <div class="modal-exercises__description">
+//       <p class="modal-exercises__name">${name}</p>
+//       <div id="rating" class="modal-exercises__rating">
+//         <div class="modal-exercises__rating-value">${rating}</div>
+//         <span class="modal-exercises__rating-star" data-value="1">&#9733;</span>
+//         <span class="modal-exercises__rating-star" data-value="2">&#9733;</span>
+//         <span class="modal-exercises__rating-star" data-value="3">&#9733;</span>
+//         <span class="modal-exercises__rating-star" data-value="4">&#9733;</span>
+//         <span class="modal-exercises__rating-star" data-value="5">&#9733;</span>
+//       </div>
+//       <div class="modal-exercises__partials">
+//         <div class="modal-exercises__partials-item">
+//           <p class="modal-exercises__partials-title">Target</p>
+//           <p class="modal-exercises__partials-value">${target}</p>
+//         </div>
+//         <div class="modal-exercises__partials-item">
+//           <p class="modal-exercises__partials-title">Body Part</p>
+//           <p class="modal-exercises__partials-value">${bodyPart}</p>
+//         </div>
+//         <div class="modal-exercises__partials-item">
+//           <p class="modal-exercises__partials-title">Equipment</p>
+//           <p class="modal-exercises__partials-value">${equipment}</p>
+//         </div>
+//         <div class="modal-exercises__partials-title">
+//           <p class="modal-exercises__partials-title">Popular</p>
+//           <p class="modal-exercises__partials-value">${popularity}</p>
+//         </div>
+//         <div class="modal-exercises__partials-item">
+//           <p class="modal-exercises__partials-title">Burned calories</p>
+//           <p class="modal-exercises__partials-value">${burnedCalories}/${time} 
+//             <span class="modal-exercises__partials-value_span">min</span>
+//           </p>
+//         </div>
+//       </div>
+//       <p class="modal-exercises__text">
+//         ${description}
+//       </p>
+//       <div class="modal-exercises__buttons">
+//       ${
+//         false
+//           ? `<button 
+//           type="button"
+//           class="modal-exercises__button-favourites unfavorite-btn"
+//           >
+//           Unfavorite
+//           <svg
+//             class="modal-exercises__button-favourites_icon"
 //             aria-label='heart'
 //             width="20"
 //             height="20"
 //           >
 //             <use href=${spriteUrl}#icon-trash></use>
-//           </svg>`;
-
-//     return;
-//   }
-
-//   // TODO: removeFavoriteCardFromLocal(cardData._id);
-
-//   favoriteButton.innerHTML = `Add to favorites
+//           </svg>`
+//           : `<button 
+//           type="button"
+//           class="modal-exercises__button-favourites
+//           add-to-favorites-btn">
+//           Add to favorites
 //           <svg
-//             class="modal-exercises__button-favourites_icon add-to-favorites-btn"
+//             class="modal-exercises__button-favourites_icon"
 //             aria-label='heart'
 //             width="20"
 //             height="20"
 //           >
 //             <use href=${spriteUrl}#icon-heart></use>
-//           </svg>`;
+//           </svg>`
+//       }
+//         </button>
+//         <button class="modal-exercises__button-rating" data-value="${_id}">Give a rating</button>
+//       </div>
+//     </div>
+//     </div>`;
+// }
 
-//   return;
-// };
+// export function handleRatingCick(id) { 
+  
+// }
+
+// // const exercisesListRef = document.getElementById('exercises-list-container');
+
+// // if (exercisesListRef) {
+// //   exercisesListRef.addEventListener('click', onExerciseListClick);
+// // }
+
+// // export function onExerciseListClick(event) {
+
+// //   if (!event.target.closest('[data-id]')) {
+// //     return;
+// //   }
+
+// //   const id = event.target.closest('[data-id]').dataset.id;
+// //   modalExercises('64f389465ae26083f39b17a5');
+// // }
+
+
+
+// // export const handleClickFavoritesBtn = cardData => {
+// //   const favoriteButton = document.querySelector(
+// //     '.modal-exercises__button-favourites'
+// //   );
+// //   const isFavorite = isFavorite(cardData._id)
+// //   if (!isFavorite) {
+// //     // TODO: addFavoriteCardToLocal(cardData);
+
+// //     favoriteButton.innerHTML = `Unfavorite
+// //     <svg
+// //             class="modal-exercises__button-favourites_icon unfavorite-btn"
+// //             aria-label='heart'
+// //             width="20"
+// //             height="20"
+// //           >
+// //             <use href=${spriteUrl}#icon-trash></use>
+// //           </svg>`;
+
+// //     return;
+// //   }
+
+// //   // TODO: removeFavoriteCardFromLocal(cardData._id);
+
+// //   favoriteButton.innerHTML = `Add to favorites
+// //           <svg
+// //             class="modal-exercises__button-favourites_icon add-to-favorites-btn"
+// //             aria-label='heart'
+// //             width="20"
+// //             height="20"
+// //           >
+// //             <use href=${spriteUrl}#icon-heart></use>
+// //           </svg>`;
+
+// //   return;
+// // };
 
