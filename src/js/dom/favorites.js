f6646b621df243b332786a343acc89cd3a4dead6
Andrii Stanislav <139786374+Andrii-Stanislav-CodeAndCakes@users.noreply.github.com>
2024-02-24 14:16:12 +0100

    fix: empty-favorites-list

--- src/js/dom/favorites.js
+++ src/js/dom/favorites.js
@@ -4,7 +4,6 @@ import renderExercisesList from './render-exercises-list';
 
 import { refs } from '../refs';
 import { addPagination } from '../helper/add-pagination.js';
-import createExercisesMarkup from './create-exercises-markup.js';
 
 const PER_PAGE = 10;
 
@@ -12,16 +11,14 @@ export const initialFavoriteList = () => {
   const favoriteExercises = favoritesStorage.getAll();
 
   if (favoriteExercises.length === 0) {
-    refs.containerFavorites.insertAdjacentHTML(
-      'afterbegin',
+    refs.containerFavorites.innerHTML =
       "<div class='favorite-exercises-empty-text-container'>" +
-        "<p class='favorite-exercises-empty-text'>" +
-        "It appears that you haven't added any exercises to your favorites" +
-        ' yet. To get started, you can add exercises that you like to your' +
-        ' favorites for easier access in the future. ' +
-        '</p>' +
-        '</div>'
-    );
+      "<p class='favorite-exercises-empty-text'>" +
+      "It appears that you haven't added any exercises to your favorites" +
+      ' yet. To get started, you can add exercises that you like to your' +
+      ' favorites for easier access in the future. ' +
+      '</p>' +
+      '</div>';
 
     return;
   }
