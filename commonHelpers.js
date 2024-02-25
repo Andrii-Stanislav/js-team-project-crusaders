ad0d612dba2a20881f8c89b17117d926c6c46e5d
Andrii-Stanislav <66518828+Andrii-Stanislav@users.noreply.github.com>
2024-02-25 11:22:39 +0000

    Deploying to gh-pages from @ Andrii-Stanislav/js-team-project-crusaders@305510663be6b2c142bb0b42415fa1a65d0097ef 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import{r as a}from"./assets/header-bc756a79.js";import"./assets/vendor-d8b4b001.js";import{f as i,i as r}from"./assets/open-exercises-modal-507f1de2.js";a.favoriteContainer.addEventListener("click",t=>{t.target.dataset.id&&Array.from(t.target.classList).includes("js-button-favorites-delete")&&(i.remove(t.target.dataset.id),r())});r();
+import{r as a}from"./assets/header-bc756a79.js";import"./assets/vendor-d8b4b001.js";import{f as i,i as r}from"./assets/open-exercises-modal-43f0cf4a.js";a.favoriteContainer.addEventListener("click",t=>{t.target.dataset.id&&Array.from(t.target.classList).includes("js-button-favorites-delete")&&(i.remove(t.target.dataset.id),r())});r();
 //# sourceMappingURL=commonHelpers.js.map
